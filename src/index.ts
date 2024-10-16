dfc987de44bac022d7674b63c08d15c32609365d
leonid-aid <leonid0aid@gmail.com>
2024-10-16 16:31:57 +0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import authRoutes from './server';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
     origin: 'http://localhost:3000', 
